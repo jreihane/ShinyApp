e140b7a84ca9752abaf67b5189e23b80fec38611
reihane zekri <reihane.zekri@gmail.com>
2014-08-20 02:55:56 +0430

    1

--- ui.R
+++ ui.R
@@ -6,47 +6,58 @@
 #
 
 library(shiny)
-data(mtcars)
 
-shinyUI(fluidPage(
-        tags$head(
-                tags$style(HTML("
-                      h1 {
-                        font-family: 'Baskerville Old Face', cursive;
-                        font-weight: 500;
-                        line-height: 1.1;
-#                         color: purple;
-                      }
-                      label {
-                        font-family: Baskerville Old Face, cursive;
-                        font-weight: 500;
-                        line-height: 1.1;
-                        font-size: 15pt;
-                      }
-                      div{
-                        display:inline-block;
-                      }
-                      span,.checkbox.inline,#formulaetxt{
-                        color: blue;
-                      }
-        
-                "))
-        ),
-        
-        headerPanel("Linear Regression with mtcars dataset"),
-        
-        sidebarPanel(width = 12,
-                span("mpg ~"),
-                textOutput(outputId = "formulaetxt"),
-                br(),br(),
-                checkboxGroupInput(inputId = "chgrp1",inline = T,
-                     label = "Select a variable to add to the regression model:",
-                     choices = list("cyl","disp","hp","drat","wt","qsec","vs","am","gear","carb")
+shinyUI(
+        fluidPage(headerPanel("Linear Regression with mtcars dataset"),
+                tags$head(
+                        tags$style(HTML("
+                                      h1 {
+                                        font-family: 'Baskerville Old Face', cursive;
+                                        font-weight: 500;
+                                        line-height: 1.1;
+                #                         color: purple;
+                                      }
+                                      label {
+                                        font-family: Baskerville Old Face, cursive;
+                                        font-weight: 500;
+                                        line-height: 1.1;
+                                        font-size: 15pt;
+                                      }
+                                      div{
+                                        display:inline-block;
+                                      }
+                                      span,.checkbox.inline,#formulaetxt{
+                                        color: blue;
+                                      }
+                        
+                                "))
+                ),
+                fluidRow(
+                        tabsetPanel(
+                                tabPanel(title="App",
+                                         fluidPage(
+                                                sidebarPanel(width = 12,
+                                                        span("mpg ~"),
+                                                        textOutput(outputId = "formulaetxt"),
+                                                        br(),br(),
+                                                        checkboxGroupInput(inputId = "chgrp1",inline = T,
+                                                             label = "Select a variable to add to the regression model:",
+                                                             choices = list("cyl","disp","hp","drat","wt","qsec","vs","am","gear","carb")
+                                                        )
+                                                )
+                                                ,mainPanel(width = 12,
+                                                           
+                                                        plotOutput(width = "100%", height = "600",outputId = "formulaePlot")
+                                                )
+                                         )
+                                )
+                                ,tabPanel(title="documentation",
+                                          fluidPage(
+#                                                   tags$script(HTML("if (window.innerHeight < 400) alert('Screen too small');"))
+                                                  tags$a(href = "index.html", "Click Here!")
+#                                                   includeHTML("www\\index.html")
+                                         )
+                                )
+                        )
                 )
-        )
-        
-        ,mainPanel(width = 12,
-                plotOutput(width = "100%", height = "600",outputId = "formulaePlot")
-                
-        )
 ))
